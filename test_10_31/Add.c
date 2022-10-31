8a0b580e03d4c629d324b2e4516b36e7023e8155
CQCQCQ <13762544373m@sina.cn>
2022-10-31 09:55:10 +0800

    函数部分更新

---
+++ test_10_31/Add.c
@@ -0,0 +1,6 @@
+ #define _CRT_SECURE_NO_WARNINGS
+int Add(int x, int y) {
+	int num = 0;
+	num = x + y;
+	return num;
+}
---
+++ test_10_31/test_10_31.c
@@ -0,0 +1,28 @@
+ #define _CRT_SECURE_NO_WARNINGS
+//函数练习（嵌套）
+#include<stdio.h>
+#include"Add.h"
+//int main() {
+//	int a, b, c = 0;
+//	printf("请输入两个整数\n");
+//	scanf("%d %d", &a, &b);
+//		c = Add(a, b);
+//		printf("%d", c);
+//	return 0;
+//}
+
+//void Swap(int* pa,int* pb) {
+//	int z = 0;
+//	z = *pa;
+//	*pa = *pb;
+//	*pb = z;
+//}
+//int main() {
+//	int a, b = 0;
+//	printf("请输入两个值\n");
+//	scanf("%d %d",&a,&b);
+//	Swap(&a, &b);
+//	printf("%d %d",a,b);
+//	return 0;
+//}
+
